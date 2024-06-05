126b0340e1f5a319ecbea91a03761764b32e4f70
callmebloom <144678597+callmebloom@users.noreply.github.com>
2024-06-05 10:16:59 +0300

    Добавьте файлы проекта.

---
+++ biblio/biblio.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+__declspec(dllexport) bool FindLetters(const char* vstroke, const char* naitibukvu) {
+	string str(vstroke);
+	string chars(naitibukvu);
+
+	for (char c : chars) {
+		if (str.find(c) == string::npos) {
+			return false;
+		}
+	}
+	return true;
+	
+}
---
+++ plusprac8/plusprac8.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+typedef bool(*FindSymvol)(const char*, const char*);
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	HINSTANCE hGetProcIDDLL = LoadLibrary(L"biblio.dll");
+	if (!hGetProcIDDLL) {
+		cout << "не загружается библиотека((" << endl;
+		return 1;
+	}
+	FindSymvol naitibukvu = (FindSymvol)GetProcAddress(hGetProcIDDLL, "FindLetters");
+	if (!naitibukvu) {
+		cout << "не загружается библиотека(((((" << endl;
+		FreeLibrary(hGetProcIDDLL);
+		return 1;
+	}
+	string vstroke, symvoly;
+	do {
+		cout << "пиши строку: ";
+		getline(cin, vstroke);
+	} 
+	while (vstroke.empty());
+
+	cout << "что тебе найти? ^^  ";
+	cin >> symvoly;
+
+	bool res = naitibukvu(vstroke.c_str(), symvoly.c_str());
+	if (res) {
+		cout << "есть такая буква! крутите барабан! (true)";
+	}
+	else {
+		cout << "нет такого символа. подарки в студию! (false)";
+	}
+	
+	FreeLibrary(hGetProcIDDLL);
+	return 0;
+}
